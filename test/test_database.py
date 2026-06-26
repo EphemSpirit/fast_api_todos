09358efc7e2f5819d4d595e98f6fe3df11833c17
Drew Hund <languageinvestigator@gmail.com>
2026-06-26 15:55:46 -0400

    move test db config

---
+++ test/test_database.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+from app.extensions import Base, get_db
+from app.utils.auth_utils import get_current_user
+from main import app
+from fastapi.testclient import TestClient
+import os
+
+
+engine = create_engine(
+    os.getenv("TEST_DATABASE_URL"),
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool
+)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def override_get_current_user():
+    return {"username": "drewhundtest", "id": 1, "role": "admin"}
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+client = TestClient(app)
